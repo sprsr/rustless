6bde19e8bb0807ee783a3b0ee2f5cde48a5605e0
sprsr <projecthourglass@protonmail.com>
2023-05-04 00:01:39 -0500

    panic impl, entry point, some linker errors

--- rustless_os/src/main.rs
+++ rustless_os/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+// No std env
+#![no_std]
+// Diverty entry point to Rust runtime
+#![no_main]
+
+//main.rs
+//No_std environment requires a panic_handler definition
+use core::panic::PanicInfo;
+//Panic Handler
+// PanicInfo contains file and line where panic happened
+// and the optional panic message.  the function won't return
+// diverging function, returning never
+// For now this routine does nothing
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+// Disable name mangling ensures compiler outputs function _start.
+// Without, compiler would generate cryptic symbols
+#[no_mangle]
+// We must extern C to tell compiler to use C calling convention
+// Returning ! ~ Diverting function.
+// Instead of return, we exit :)
+pub extern "C" fn_start() -> ! {
+    loop {}
+}
+
---
+++ rustless_os/target/debug/deps/rustless_os-f07ceffd98dc9656.d
@@ -0,0 +1,5 @@
+/home/sp/repos/rustless/rustless_os/target/debug/deps/rustless_os-f07ceffd98dc9656: src/main.rs
+
+/home/sp/repos/rustless/rustless_os/target/debug/deps/rustless_os-f07ceffd98dc9656.d: src/main.rs
+
+src/main.rs:
