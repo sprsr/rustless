6330beac805ea734860818522f5bcb954768f54c
sprsr <projecthourglass@protonmail.com>
2023-05-21 12:48:54 -0500

    newline

--- rustless_os/src/vga_buffer.rs
+++ rustless_os/src/vga_buffer.rs
@@ -87,8 +87,16 @@ impl Writer {
         }
     }
    
-    fn new_line(&mut self) { }
-
+    fn new_line(&mut self) { 
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
     // Function to write a full string
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
