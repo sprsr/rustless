970964f1eb99cc4b5c9f78ae602651b6a61af3bc
sprsr <projecthourglass@protonmail.com>
2023-05-28 10:18:11 -0500

    adding test runner

--- rustless_os/src/main.rs
+++ rustless_os/src/main.rs
@@ -51,3 +51,13 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]){
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
